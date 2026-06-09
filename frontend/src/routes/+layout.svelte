4f6eb01b7c7a5cdb76701bb06e726c0048da5fb6
Oliver Sepp <olsepp@taltech.ee>
2026-06-09 15:22:11 +0300

    new icon

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/icon.svg';
 	import { isAuthenticated } from '$lib/stores/auth.store';
 	import { goto } from '$app/navigation';
 	import { resolve } from '$app/paths';
